da49e2d4ed7b4305580705d5f626a5711cb895a6
Rajat Jacob <rajatjacob@gmail.com>
2024-02-22 01:33:26 +0530

    Upload UI

--- app.py
+++ app.py
@@ -18,26 +18,26 @@ def upload():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
-    file = request.files['file']
+    files = request.files.getlist("file")
+    all_files = set([file.filename for file in files])
+    success = set()
+    for file in files:
+        if file.filename == '':
+            pass
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
-    if file and file.filename.endswith('.pdf'):
-        # You can save the file to a location
-        file_path = 'papers/' + file.filename
-        file.save(file_path)
-        try:
-            get_paper_details(file_path=file_path)
-            add_file_to_store(file_path=file_path)
-        except Exception as e:
-            print("ERROR", e)
-            return jsonify({'error': 'Couldn\'t process PDF file.'}), 500
-        return 'File uploaded successfully'
-    else:
-        return jsonify({
-            'error': 'Invalid file format. Please upload a PDF file'
-        }), 400
+        if file and file.filename and file.filename.endswith('.pdf'):
+            file_path = 'papers/' + file.filename
+            file.save(file_path)
+            try:
+                get_paper_details(file_path=file_path)
+                add_file_to_store(file_path=file_path)
+                success.add(file.filename)
+            except Exception:
+                pass
+    return jsonify({
+        'successful': list(success),
+        'failed': list(all_files-success)
+    })
 
 
 @app.route('/ama', methods=['GET'])
